29d694f296299915debca8a516ef1cbe657ae439
jbenzer <136658052+jbenzer@users.noreply.github.com>
2023-07-13 12:56:48 +0000

    Pending changes exported from your codespace

--- app.rb
+++ app.rb
@@ -7,3 +7,25 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/howdy") do
+  hi
+
+  erb(:hello)
+end
+
+get("/goodbye") do
+  erb(:bye)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("square/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 2
+
+  erb(:square_results)
+end
